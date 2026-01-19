3a7a99a2644cbe2914d3f961abe66a93fc6777fb
Aakarsh Kashyap <souls.syntax@gmail.com>
2026-01-20 00:41:20 +0530

    made scrolling more efficeint i guess

--- src/kernel/drivers/video/scroll.c
+++ src/kernel/drivers/video/scroll.c
@@ -2,6 +2,7 @@
 #include <video.h>
 #include <limine.h>
 #include <stdint.h>
+#include <string.h>
 
 void framebuffer_scroll(void) {
   uint8_t *fb = (uint8_t * )g_framebuffer->address;
@@ -11,11 +12,14 @@ void framebuffer_scroll(void) {
   uint64_t scroll_bytes = 16 * pitch;
   uint64_t total_bytes  = height * pitch;
 
-  for(uint64_t i = scroll_bytes; i < total_bytes; i++) {
-    fb[i - scroll_bytes] = fb[i];
-  }
-
-  for(uint64_t i = total_bytes - scroll_bytes; i < total_bytes; i++) {
-    fb[i] = 0x00;
-  }
+  memmove(fb, fb + scroll_bytes, total_bytes - scroll_bytes);
+  memset(fb + total_bytes - scroll_bytes, 0, scroll_bytes);
+  //
+  // for(uint64_t i = scroll_bytes; i < total_bytes; i++) {
+  //   fb[i - scroll_bytes] = fb[i];
+  // }
+  //
+  // for(uint64_t i = total_bytes - scroll_bytes; i < total_bytes; i++) {
+  //   fb[i] = 0x00;
+  // }
 }
